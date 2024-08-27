687db3ef3146797b0241cb28ec2a6450cb6110de
Gabriel Dantas <100931688+gabrieldantas07@users.noreply.github.com>
2024-08-27 09:43:27 -0300

    Add files via upload

--- exercicio/conexao.php
+++ exercicio/conexao.php
@@ -1,15 +1,15 @@
 <?php
-    $host = 'localhost'; //aonde está a base de dados
-    $usuario = 'root'; //quem é o usuário do banco de dados
+    $host = 'localhost';
+    $usuario = 'root';
     $senha = '';
-    $banco = 'etecmcm'; //nome do banco
+    $banco = 'etecmcm';
 
-    $conexao = new mysqli($host, $usuario, $senha, $banco);
+    $conexao = new mysqli($host,$usuario,$senha,$banco);
 
-    if($conexao->connect_error){ 
-        die('Falha de conexão: ' . $conexao-> connect_error);
+    if($conexao->connect_error){
+        die('Falha de conexão: ' . $conexao->connect_error);
     }
-    //else{
-    //    echo "CONECTADO COM SUCESSO!";
+    // else{
+    //     echo "CONECTADO COM SUCESSO!";
     // }
 ?>
